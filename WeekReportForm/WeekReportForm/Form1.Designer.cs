7b68b2c4e0a5fbda8090727138d5d5cbbebf3adf
yukibacho <57794992+yukibacho@users.noreply.github.com>
2020-01-10 16:27:31 +0900

    FirstCommit

---
+++ WeekReportForm/WeekReportForm/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace WeekReportForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Location = new System.Drawing.Point(2, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(717, 401);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.SeaShell;
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(709, 375);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "1日目";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.SeaShell;
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(709, 381);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "2日目";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.SeaShell;
+            this.tabPage3.Controls.Add(this.textBox3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(709, 381);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "3日目";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.SeaShell;
+            this.tabPage4.Controls.Add(this.textBox4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(709, 381);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "4日目";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.SeaShell;
+            this.tabPage5.Controls.Add(this.textBox5);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(709, 381);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "5日目";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(626, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 7);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(692, 367);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(8, 7);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(692, 367);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(8, 7);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(692, 367);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(8, 7);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(692, 367);
+            this.textBox4.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(8, 7);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox5.Size = new System.Drawing.Size(692, 367);
+            this.textBox5.TabIndex = 1;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.BackColor = System.Drawing.Color.SeaShell;
+            this.tabPage6.Controls.Add(this.textBox6);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(709, 381);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "来週の予定";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(8, 7);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox6.Size = new System.Drawing.Size(692, 367);
+            this.textBox6.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(80, 417);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(140, 19);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "作成日：";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(626, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "クリア";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(716, 445);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "週報作成アプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WeekReportForm/WeekReportForm/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WeekReportForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 各テキストボックスの値を取得
+            string text1 = textBox1.Text;
+            string text2 = textBox2.Text;
+            string text3 = textBox3.Text;
+            string text4 = textBox4.Text;
+            string text5 = textBox5.Text;
+            string next = textBox6.Text;
+
+            string filepath = "";
+            DateTime Today = dateTimePicker1.Value;
+            string day = Today.Month.ToString() + "/" + Today.Day.ToString();
+
+            FolderBrowserDialog fbDialog = new FolderBrowserDialog();
+
+            // ダイアログの説明文を指定する
+            fbDialog.Description = "週報保存先フォルダを選択";
+
+            // デフォルトのフォルダを指定する
+            fbDialog.SelectedPath = @"C:";
+
+            // 「新しいフォルダーの作成する」ボタンを表示する
+            fbDialog.ShowNewFolderButton = true;
+
+            //フォルダを選択するダイアログを表示する
+            if (fbDialog.ShowDialog() == DialogResult.OK)
+            {
+                filepath = fbDialog.SelectedPath + @"\";
+            }
+            else
+            {
+                return;
+            }
+            // ファイル名決定
+            string filename = filepath + 
+                Today.Year.ToString() + 
+                CheckNum(Today.Month.ToString()) +
+                CheckNum(Today.Day.ToString()) +
+                "_週報.txt";
+
+            List<int> dayList = WeekCheck(Today.DayOfWeek);
+
+            if (dayList[0] == -1)
+            {
+                MessageBox.Show("休日に週報は作成できません。");
+                return;
+            }
+
+            DateTime first = Today.AddDays(-dayList[0]);
+            string firstday = first.Month.ToString() + "/" + first.Day.ToString();
+
+            DateTime second = Today.AddDays(-dayList[1]);
+            string secondday = second.Month.ToString() + "/" + second.Day.ToString();
+
+            DateTime third = Today.AddDays(-dayList[2]);
+            string thirdday = third.Month.ToString() + "/" + third.Day.ToString();
+
+            DateTime fourth = Today.AddDays(-dayList[3]);
+            string fourthday = fourth.Month.ToString() + "/" + fourth.Day.ToString();
+
+
+            // タイトル作成
+            StringBuilder sb = new StringBuilder();
+            sb.Append("【週報】 " + firstday + "～" + day + "\n");
+
+            // 今週の報告
+            sb.Append("■今週の報告\n");
+            sb.Append(firstday + " " + text1 + "\n\n");
+            sb.Append(secondday + " " + text2 + "\n\n");
+            sb.Append(thirdday + " " + text3 + "\n\n");
+            sb.Append(fourthday + " " + text4 + "\n\n");
+            sb.Append(day + " " + text5 + "\n\n");
+            sb.Append("■来週の取り組み\n");
+            sb.Append(next);
+
+            File.AppendAllText(filename,sb.ToString());
+            MessageBox.Show("週報が作成されました。");
+
+        }
+        private List<int> WeekCheck(DayOfWeek dow)
+        {
+            List<int> retList = new List<int>();
+            switch (dow)
+            {
+                case DayOfWeek.Monday:
+                    retList.Add(6);
+                    retList.Add(5);
+                    retList.Add(4);
+                    retList.Add(3);
+                    break;
+                case DayOfWeek.Tuesday:
+                    retList.Add(6);
+                    retList.Add(5);
+                    retList.Add(4);
+                    retList.Add(1);
+                    break;
+                case DayOfWeek.Wednesday:
+                    retList.Add(6);
+                    retList.Add(5);
+                    retList.Add(2);
+                    retList.Add(1);
+                    break;
+                case DayOfWeek.Thursday:
+                    retList.Add(6);
+                    retList.Add(3);
+                    retList.Add(2);
+                    retList.Add(1);
+                    break;
+                case DayOfWeek.Friday:
+                    retList.Add(4);
+                    retList.Add(3);
+                    retList.Add(2);
+                    retList.Add(1);
+                    break;
+                default:
+                    retList.Add(-1);
+                    break;
+            }
+            return retList;
+        }
+
+        private string CheckNum(string i)
+        {
+            string str = "";
+
+            if (i.Length == 1)
+            {
+                str = "0" + i;
+            }
+            else
+            {
+                str = i;
+            }
+            return str;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.ResetText();
+            textBox2.ResetText();
+            textBox3.ResetText();
+            textBox4.ResetText();
+            textBox5.ResetText();
+            textBox6.ResetText();
+        }
+    }
+}
